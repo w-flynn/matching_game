1b57634ffe7bb07cc4aafe8230bd0e8f2cae8ba6
willreichardflynn <wreichardflynn@gmail.com>
2021-06-10 10:27:18 -0400

    First commit

---
+++ assets/scripts/cards.js
@@ -0,0 +1,36 @@
+let images = [] //fill with paths to images
+
+let deck = []
+
+function Card(id,image_path) {
+    this.id = id,
+    this.loc = null,
+    this.flipped = false,
+    this.image = image_path
+}
+
+Card.prototype = {
+    flip: function() {
+        if (this.flipped === true) {
+            this.flipped = false;
+            //JS to change img src to stock background
+        }
+        else {
+            this.flipped = true;
+            //JS to change img src to this.image
+        }
+    }
+    place: function
+}
+
+for (let i=0; i < images.length; i++) {
+    deck.push(new Card(i,images[i]))
+}
+
+function deal {
+    let count = 0;
+    for (let i=0; i < images.length; i++) {
+        
+    }
+}
+
